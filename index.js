312dd548e5d9e64e4c3373a4a887e25a62eee2e9
Alena <alenzija@gmail.com>
2023-04-30 09:27:28 +0200

    refactor: improve class keyboard and index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import CharacterKey from './modules/character-key.js';
-import ModifierKey from './modules/modifier-key.js';
 import Keyboard from './modules/keyboard.js';
 import TextArea from './modules/text-area.js';
-
-const res = await fetch('keys.json');
-const keys = await res.json();
-
-const keysClasses = [];
 
 const lang = 'en';
 let isShift = false;
@@ -25,29 +18,46 @@ textArea.onFocus();
 document.querySelector('textarea').addEventListener('blur', () => textArea.onFocus());
 document.addEventListener('keyup', () => { textArea.cursor = textArea.textArea.selectionStart; });
 
-keys.forEach((element) => {
-  let key;
-  switch (element.type) {
-    case 'character': {
-      key = new CharacterKey(element);
-      key.initLanguage(lang);
-      break;
-    }
-    case 'modifier': {
-      key = new ModifierKey(element);
-      break;
-    }
-    case 'control': {
-      break;
-    }
-    default: throw Error('There isn\'t this type');
-  }
-  keysClasses.push(key);
-});
+const keyboard = new Keyboard(lang, keysOn, isCapsLock);
+await keyboard.init();
+container.append(keyboard.render());
 
-const keyboard = new Keyboard(lang, keysOn, keysClasses);
+const onActiveKey = (event) => {
+  const keyItems = document.querySelectorAll('.key');
+  keyItems.forEach((key) => {
+    if (key.dataset.code === event.code) {
+      if (event.code === 'Tab') {
+        event.preventDefault();
+        setTimeout(() => { key.classList.remove('active'); }, 200);
+      }
+      key.classList.add('active');
+    }
+  });
+};
 
-function changeValue(event) {
+const offActiveKey = (event) => {
+  const keyItems = document.querySelectorAll('.key');
+  keyItems.forEach((key) => {
+    if (key.dataset.code === event.code) {
+      key.classList.remove('active');
+    }
+  });
+};
+
+const doActiveCapsLock = () => {
+  const keyItems = container.querySelectorAll('.key');
+  keyItems.forEach((key) => {
+    if (key.dataset.code === 'CapsLock') {
+      if (isCapsLock) {
+        key.classList.add('active');
+      } else {
+        key.classList.remove('active');
+      }
+    }
+  });
+};
+
+const changeValue = (event) => {
   const target = event.target.closest('.key');
   if (!target) return;
   const { code } = target.dataset;
@@ -59,73 +69,40 @@ function changeValue(event) {
   if (code === 'Backspace') {
     textArea.backspace();
   } else {
-    keysClasses.forEach((key) => {
+    keyboard.keys.forEach((key) => {
       if (key.code === code && key.type === 'character') {
         textArea.addValue(key.onClick(isShift));
       }
     });
   }
-}
+};
 
-function doActiveCapsLock() {
-  const keyItems = container.querySelectorAll('.key');
-  keyItems.forEach((key) => {
-    if (key.dataset.code === 'CapsLock') {
-      if (isCapsLock) {
-        key.classList.add('active');
-      } else {
-        key.classList.remove('active');
-      }
-    }
-  });
-}
-
-container.append(keyboard.render());
 document.querySelector('.keys').addEventListener('click', changeValue);
 
 document.addEventListener('keydown', (e) => {
-  // textArea.onFocus();
   keysOn.add(e.code);
+  if (e.key === 'Tab') textArea.tab();
   if (e.key === 'Alt') e.preventDefault();
   if (e.code === 'ShiftLeft' && keysOn.has('ControlLeft')) {
     keyboard.changeLanguage(lang);
-    document.querySelector('.keys').remove();
     container.append(keyboard.render());
     doActiveCapsLock();
     document.querySelector('.keys').addEventListener('click', changeValue);
   } else if (e.key === 'Shift' && !isShift) {
     isShift = true;
     keyboard.toggleCapsLock();
-    document.querySelector('.keys').remove();
     container.append(keyboard.render());
     doActiveCapsLock();
     document.querySelector('.keys').addEventListener('click', changeValue);
   }
-
-  const keyItems = container.querySelectorAll('.key');
-  keyItems.forEach((key) => {
-    if (key.dataset.code === e.code) {
-      if (e.code === 'Tab') {
-        e.preventDefault();
-        textArea.tab();
-        setTimeout(() => { key.classList.remove('active'); }, 200);
-      }
-      key.classList.add('active');
-    }
-  });
+  onActiveKey(e);
 });
 
 document.addEventListener('keyup', (e) => {
-//  textArea.onFocus();
   keysOn.delete(e.code);
-  const keyItems = container.querySelectorAll('.key');
-  keyItems.forEach((key) => {
-    if (key.dataset.code === e.code) {
-      key.classList.remove('active');
-    }
-  });
+  offActiveKey(e);
 
-  keysClasses.forEach((key) => {
+  keyboard.keys.forEach((key) => {
     if (key.type === 'character' && key.code === e.code) {
       textArea.addValue(key.onClick(isShift));
     }
@@ -134,7 +111,6 @@ document.addEventListener('keyup', (e) => {
   if (e.key === 'Shift' && isShift) {
     isShift = false;
     keyboard.toggleCapsLock();
-    document.querySelector('.keys').remove();
     container.append(keyboard.render());
     doActiveCapsLock();
     document.querySelector('.keys').addEventListener('click', changeValue);
@@ -143,11 +119,8 @@ document.addEventListener('keyup', (e) => {
   if (e.key === 'CapsLock') {
     isCapsLock = !isCapsLock;
     keyboard.toggleCapsLock();
-    document.querySelector('.keys').remove();
     container.append(keyboard.render());
     doActiveCapsLock();
     document.querySelector('.keys').addEventListener('click', changeValue);
   }
 });
-
-// Смена языка
--- modules/keyboard.js
+++ modules/keyboard.js
@@ -1,9 +1,37 @@
+import CharacterKey from './character-key.js';
+import ModifierKey from './modifier-key.js';
+
 export default class Keyboard {
-  constructor(lang, setOnKeys, keys, isCapsLock = false) {
+  constructor(lang, setOnKeys, isCapsLock) {
+    this.keyboard = document.createElement('div');
     this.lang = lang;
     this.setOnKeys = setOnKeys;
-    this.keys = keys;
+    this.keys = [];
     this.isCapsLock = isCapsLock;
+  }
+
+  async init() {
+    const res = await fetch('keys.json');
+    const keys = await res.json();
+    keys.forEach((item) => {
+      let key;
+      switch (item.type) {
+        case 'character': {
+          key = new CharacterKey(item);
+          key.initLanguage(this.lang);
+          break;
+        }
+        case 'modifier': {
+          key = new ModifierKey(item);
+          break;
+        }
+        case 'control': {
+          break;
+        }
+        default: throw Error('There isn\'t this type');
+      }
+      this.keys.push(key);
+    });
   }
 
   changeLanguage() {
@@ -21,8 +49,8 @@ export default class Keyboard {
   }
 
   render() {
-    const keysContainer = document.createElement('div');
-    keysContainer.className = 'keys';
+    this.keyboard.innerHTML = '';
+    this.keyboard.className = 'keys';
     this.keys.forEach((key) => {
       switch (key.type) {
         case 'character': {
@@ -36,8 +64,8 @@ export default class Keyboard {
         }
         default: throw new Error('this type there aren\'t');
       }
-      keysContainer.append(key.render());
+      this.keyboard.append(key.render());
     });
-    return keysContainer;
+    return this.keyboard;
   }
 }
